1fc597008f35f417987624769095fde40213ddd3
Keeva Connolly <119996929+keeva-c@users.noreply.github.com>
2026-07-28 15:52:21 +1000

    Increase memory allocation to 32g for genome assembly

--- profiles/pawsey/20_genomeassembly.sh
+++ profiles/pawsey/20_genomeassembly.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=4g
+#SBATCH --mem=32g
 #SBATCH --time=1-00
 #SBATCH --output=logs/slurm/genomeassembly.%j.out
 #SBATCH --error=logs/slurm/genomeassembly.%j.err
